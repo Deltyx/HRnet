1553cb2b2fed97acdfe0a5c4142b8c7b17156672
Alexis <alexismorin.pro@gmail.com>
2022-12-03 19:43:41 +0100

    modal WIP

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,12 +1,16 @@
-
+import './modal.scss'
 
 export function Modal(props) {
 
     return (
-        <div className="modal">
-            <button onClick={props.onClose}>X</button>
-            <div className="modal-content">
-                {props.content}
+        <div className='modal-wrapper'>
+            <div className="modal">
+                <div className='modal-header'>
+                    <h1 className="modal-title">
+                        {props.content}
+                    </h1>
+                    <button onClick={props.onClose}>X</button>
+                </div>
             </div>
         </div>
     )
